3f35a7d5d5084f0ea241d9077a526f33aa9491f1
Camila <camigeneral@gmail.com>
2026-03-19 14:29:53 -0300

    Modifico gitignore

--- input-files/file1.py
+++
@@ -1,8 +0,0 @@
-def generador_de_numero():
-  return 34
-
-def numero_mas_cinco(numero):
-  return numero + 5
-
-def sumar_cinco_llamando_funcion():
-  return numero_mas_cinco(generador_de_numero())
