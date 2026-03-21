de0324ccebbb675728bd7577eb477c57614057e9
Camila <camigeneral@gmail.com>
2026-03-21 18:08:27 -0300

    Guardo la posiciion de las funciones, clases y imports

--- src/models/function_info.rs
+++ src/models/function_info.rs
@@ -4,6 +4,7 @@ use crate::models::{function_call::FunctionCall, parameter_info::ParameterInfo};
 #[derive(Debug, Serialize)]
 pub struct FunctionInfo {
     pub name: String,
+    pub line: usize,
     pub parameters: Vec<ParameterInfo>,
     pub return_type: Option<String>,
     pub function_calls: Option<Vec<FunctionCall>>
